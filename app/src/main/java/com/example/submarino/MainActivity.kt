ca395e213ddb5742f292ff861d726f08e71d8653
Carlos <juancaclavijo2005@gmail.com>
2024-03-27 19:32:24 -0500

    Añadida Conexion Bluetooth

--- app/src/main/java/com/example/submarino/MainActivity.kt
+++ app/src/main/java/com/example/submarino/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.submarino
 
-
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,40 +28,92 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
 import com.example.submarino.ui.theme.SubmarinoTheme
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
-
 class MainActivity : ComponentActivity() {
+    private val REQUEST_ENABLE_BT = 1
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+    //FUNCION PRINCIPAL
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if(Build.VERSION.SDK_INT > 30){
+                    val permission = arrayOf(Manifest.permission.BLUETOOTH_CONNECT)
+                    ActivityCompat.requestPermissions(this, permission, REQUEST_ENABLE_BT)
+                }
+                else{
+                    val permission = arrayOf(Manifest.permission.BLUETOOTH_ADMIN)
+                    ActivityCompat.requestPermissions(this, permission, REQUEST_ENABLE_BT)
+                }
+            }
+            bluetoothManager = getSystemService(BluetoothManager::class.java)
+            bluetoothAdapter = bluetoothManager.adapter
+            if (!bluetoothAdapter.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
             SubmarinoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.LightGray
                 ) {
-                    Radar(400f)
+                    Radar(size = 400f)
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Radar(size: Float, angulo: Double = 0.0, modifier: Modifier = Modifier) {
+fun Radar(modifier: Modifier = Modifier, size: Float) {
+    val angular = 0.0
     var sliderPosition by remember { mutableFloatStateOf(0f) }
-    Column {
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row (verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+            modifier = modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(color = Color.Gray)
+                .padding(4.dp)
+        ) {
+            Text(
+                text = "Sistema Control Submarino",
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = "Conectar"
+                )
+            }
+        }
         Slider(
             value = sliderPosition / 360f,
             onValueChange = { sliderPosition = it * 360f}
@@ -96,8 +158,8 @@ fun Radar(size: Float, angulo: Double = 0.0, modifier: Modifier = Modifier) {
             onDraw = {
                 val center = Offset(size.dp.toPx() / 2f, size.dp.toPx() / 2f)
                 val maxRad = ((size - 2f) / 2f)
-                var a = angulo - (PI / 3)
-                while (a < angulo) {
+                var a = angular - (PI / 3)
+                while (a < angular) {
                     val destino = Offset(
                         (center.x + (maxRad.dp.toPx() * cos(a))).toFloat(),
                         (center.y - (maxRad.dp.toPx() * sin(a))).toFloat()
@@ -107,7 +169,7 @@ fun Radar(size: Float, angulo: Double = 0.0, modifier: Modifier = Modifier) {
                             0,
                             255,
                             0,
-                            alpha = (200 - (3.33 * ((angulo - a) * (180.0 / PI)))).toInt()
+                            alpha = (200 - (3.33 * ((angular - a) * (180.0 / PI)))).toInt()
                         ), center, destino, strokeWidth = 1.dp.toPx()
                     )
                     a += (PI / 180)
@@ -120,6 +182,6 @@ fun Radar(size: Float, angulo: Double = 0.0, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SubmarinoTheme {
-        Radar(400f)
+        Radar(size= 400f)
     }
 }
